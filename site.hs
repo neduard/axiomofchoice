55321f4882f141d46a087eff2f0ac9fb8bdd1f7b
Eduard Nicodei <eddnicodei@gmail.com>
2020-10-26 15:12:24 +0000

    Fix site.sh and remove difference between techblock and journal

--- site.hs
+++ site.hs
@@ -6,7 +6,7 @@ import qualified GHC.IO.Encoding as E
 import           System.Exit (exitWith)
 
 
-techblog = 
+website =
     hakyllWithExitCode config $ do
         match "images/*" $ do
             route   idRoute
@@ -58,47 +58,13 @@ techblog =
 
         match "templates/*" $ compile templateBodyCompiler
     where config = defaultConfiguration { destinationDirectory = "_main"
-                                        , deployCommand = "rsync -rav _main/ granitefelt:/var/www/htdocs/axiomofchoice.xyz/ "}
-
-journal = 
-    hakyllWithExitCode config $ do
-        match "images/*" $ do
-            route   idRoute
-            compile copyFileCompiler
-
-        match "css/*" $ do
-            route   idRoute
-            compile compressCssCompiler
-
-        match "journal/*" $ do
-            route $ setExtension "html"
-            compile $ pandocCompiler
-                >>= loadAndApplyTemplate "templates/post.html"    postCtx
-                >>= loadAndApplyTemplate "templates/default.html" postCtx
-                >>= relativizeUrls
-
-        match "index.html" $ do
-            route idRoute
-            compile $ do
-                posts <- recentFirst =<< loadAll "journal/*"
-                let indexCtx =
-                        listField "posts" postCtx (return posts) `mappend`
-                        defaultContext
-                getResourceBody
-                    >>= applyAsTemplate indexCtx
-                    >>= loadAndApplyTemplate "templates/default.html" indexCtx
-                    >>= relativizeUrls
-
-        match "templates/*" $ compile templateBodyCompiler
-    where config = defaultConfiguration { destinationDirectory = "_journal"
-                                         , deployCommand = "rsync -rav _journal/ granitefelt:/var/www/htdocs/journal.axiomofchoice.xyz/ "}
+                                        , deployCommand = "rsync -rav _main/ root@axiomofchoice.xyz:/var/www/htdocs/axiomofchoice.xyz/ "}
 
 --------------------------------------------------------------------------------
 main :: IO ()
 main = do
         E.setLocaleEncoding E.utf8
-        _ <- techblog
-        journal >>= System.Exit.exitWith
+        website >>= System.Exit.exitWith
 
 --------------------------------------------------------------------------------
 postCtx :: Context String
